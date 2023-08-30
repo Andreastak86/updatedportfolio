2054458e46460101a4c50208ce0b51d8b9cade62
Andreas Takvam <104064121+Andreastak86@users.noreply.github.com>
2023-08-30 15:56:44 +0200

    remove cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import CV from "../../assets/CV Andreas Takvam 2022.pdf";
 
 const CTA = () => {
-  return (
-    <div className='cta'>
-      <a href={CV} download className='btn'>
+    return (
+        <div className='cta'>
+            {/* <a href={CV} download className='btn'>
         Last Ned CV
-      </a>
-      <a href='#contact' className='btn'>
-        Kontakt Meg
-      </a>
-    </div>
-  );
+      </a> */}
+            <a href='#contact' className='btn'>
+                Kontakt Meg
+            </a>
+        </div>
+    );
 };
 
 export default CTA;
